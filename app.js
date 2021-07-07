1882d10a18eb1649bb82d4fc27a0d541e945999c
Antoine <antoinecoulon@hotmail.com>
2021-07-07 17:01:30 +0200

    begin of front-end for welcome and dashboard pages

--- app.js
+++ app.js
@@ -1,45 +1,10 @@
-// // lib and imports
-// const express = require("express");
-// const app = express();
-// const router = express.Router();
-// const myFirstCOntroller = require("./controllers/controller")
-// const mongoose = require('mongoose');
-// const expressEjsLayout = require('express-ejs-layouts')
-
-// //mongoose
-// mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
-// .then(() => console.log('connected,,'))
-// .catch((err)=> console.log(err));
-
-// // app setup
-// app.use(express.json())
-// app.use("/static", express.static("public"));
-// app.set("view engine", "ejs");
-// app.use(expressEjsLayout);
-
-// //BodyParser
-// app.use(express.urlencoded({extended : false}));
-
-// //Routes
-// app.use('/',require('./routes/index'));
-// app.use('/users',require('./routes/users'));
-
-// // pages
-// app.get('/',(req, res) => {
-//   res.render('home.ejs');
-// });
-
-// // Create here your api setup
-
-
-// app.listen(3000, () => console.log("Server Up and running"));
 
 //APP.JS in ~/app.js
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const app = express();
-const expressEjsLayout = require('express-ejs-layouts')
+// const expressEjsLayout = require('express-ejs-layouts')
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('passport');
@@ -51,8 +16,9 @@ mongoose.connect('mongodb+srv://antoinecln:Becode@bookface.dve24.mongodb.net/myF
 .catch((err)=> console.log(err));
 
 //EJS
+app.use("/static", express.static("public"));
 app.set('view engine','ejs');
-app.use(expressEjsLayout);
+// app.use(expressEjsLayout);
 
 //BodyParser
 app.use(express.urlencoded({extended : false}));
