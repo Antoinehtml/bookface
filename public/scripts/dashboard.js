da2b6273292eb411c6a6dfc7147fd35e3b5fd7e8
Antoine <antoinecoulon@hotmail.com>
2021-07-12 17:00:40 +0200

    animation js to dashboard

---
+++ public/scripts/dashboard.js
@@ -0,0 +1,7 @@
+const profile = document.getElementById('prof')
+
+profile.addEventListener('click', (event) => {
+	let logout = document.getElementById('logout');
+  	logout.classList.toggle('hidden')
+})
+
