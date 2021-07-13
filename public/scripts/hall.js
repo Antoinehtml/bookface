4b41c751c2ea5e72be66996a6ceeba2f7f9ee504
Antoine <antoinecoulon@hotmail.com>
2021-07-13 09:34:48 +0200

    front-end done for hall and dashboard, responsiveness too

---
+++ public/scripts/hall.js
@@ -0,0 +1,8 @@
+const profile = document.getElementById('prof')
+
+profile.addEventListener('click', (event) => {
+	let logout = document.getElementById('logout');
+  	logout.classList.toggle('hidden')
+})
+
+
